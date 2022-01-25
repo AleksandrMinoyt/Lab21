484a09f514eaa6de6cb1a1c079c23d8e2dd0a842
fox_iAtleksandr <fox_it@bk.ru>
2022-01-25 16:28:31 +0300

    lock print

--- Lab21/Program.cs
+++ Lab21/Program.cs
@@ -5,10 +5,8 @@ namespace Lab21
 {
     class Program
     {
-
         static byte[,] array = new byte[15, 15];
         static object lck = new object();
-
 
         static void Main(string[] args)
         {
@@ -20,21 +18,23 @@ namespace Lab21
             {
                 for (int j = 0; j < 15; j++)
                 {
-                    int maxVal = j * 16; 
-                    array[i, j] = (byte)rnd.Next(4, 4+maxVal);  // время на обработку 1 участка, чем праве, тем дольше...
+                    int maxVal = (15-j)*16; 
+                    array[i, j] = (byte)rnd.Next(4, 4+maxVal);  // время на обработку 1 участка, чем ниже, тем дольше...
                 }
-
             }
+
+            Print();
+
+            Console.ReadKey();
 
             ThreadStart threadStart1 = new ThreadStart(gardener1);
             Thread thread1 = new Thread(threadStart1);
             
             ThreadStart threadStart2 = new ThreadStart(gardener2);
-            Thread thread2 = new Thread(threadStart2);
+            Thread thread2 = new Thread(threadStart2);    
 
             thread1.Start();
-            thread2.Start();           
-
+            thread2.Start();   
             
             Console.ReadKey();
         }
@@ -51,8 +51,9 @@ namespace Lab21
                         del = array[i, j];
                         array[i, j] = 1;
                         Print();
+                        Thread.Sleep(del*3);// первый садовник начинающий, ему нужно в 3 раза больше времени
                     }
-                    Thread.Sleep(del);                 
+                                  
                 }                
                
             }
@@ -74,8 +75,9 @@ namespace Lab21
                         del = array[i, j];
                         array[i, j] = 2;
                         Print();
+                        Thread.Sleep(del); 
                     }
-                    Thread.Sleep(del*3); // второй садовник начинающий, ему нужно в 3 раза больше времени
+                   
                 }
             }
             lock (lck)
